67064c622b087ff3cfa012cdc01ea369b57fb5b9
Khantti Nyane <khanttinyane2014.14@gmail.com>
2025-01-04 04:53:12 +0630

    desktop

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import './App.css'
 import {Navbar, Footer, Sidebar, ThemeSettings} from './components'
 import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, Line, ColorMapping, Editor} from './pages'
+import { useStateContext } from './contexts/ContextProvider'
 
 const App = () => {
+    const {activeMenu, setActiveMenu} = useStateContext();
+    // const activeMenu = false;
 
-    const activeMenu = false;
   return (
     <BrowserRouter>
         <div className='flex relative dark:bg-main-dark-bg'>
